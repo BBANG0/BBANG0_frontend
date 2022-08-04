49008d0be4e52ea6d74ec794dbdd7520686f3ffd
이민섭 <minsoplee2015@gmail.com>
2022-08-04 22:05:58 +0900

    initial storyboards

---
+++ BBANG0_IOS_Study/Map/BakeryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BakeryTableViewCell.swift
+//  BBANG0_IOS_Study
+//
+//  Created by 이민섭 on 2022/08/04.
+//
+
+import UIKit
+
+class BakeryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ BBANG0_IOS_Study/Map/MapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapViewController.swift
+//  BBANG0_IOS_Study
+//
+//  Created by 이민섭 on 2022/08/04.
+//
+
+//네이버 지도 띄우기 
+
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
